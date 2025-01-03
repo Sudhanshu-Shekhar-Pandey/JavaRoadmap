0e0f85497e9cde040e2220af97efca917e9e37f8
Sudhanshu <sudhanshu0pandey@gmail.com>
2025-01-03 12:18:59 +0530

    convert Spring-MVC to Spring-Boot and adding auto configs

---
+++ SpringWebXMLMaven/src/main/java/sud/learn/ApplicationStarter.java
@@ -0,0 +1,19 @@
+package sud.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApplicationStarter {
+
+	public static void main(String[] args) {
+		
+		SpringApplication springApplication = new SpringApplication(ApplicationStarter.class);
+		springApplication.run(args);
+	}
+	
+
+	
+
+}
