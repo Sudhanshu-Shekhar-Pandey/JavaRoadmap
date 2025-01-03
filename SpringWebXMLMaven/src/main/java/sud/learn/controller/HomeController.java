2529b0e1fc4701b557350b42c2f3c032d046f513
Sudhanshu <sudhanshu0pandey@gmail.com>
2025-01-03 11:00:14 +0530

    Convert to Spring MVC project with full Java Annotation

--- SpringWebXMLMaven/src/main/java/sud/learn/controller/HomeController.java
+++ SpringWebXMLMaven/src/main/java/sud/learn/controller/HomeController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 	
-	@ResponseBody
-	@GetMapping("/about")
+//	@ResponseBody
+	@GetMapping("/home")
 	public String showAboutPage() {
-		return "This is about page" ;
+		return "home-page" ;
 	}
 
 }
