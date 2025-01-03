d40f3545859fe8dd0f35c26ae7093c5a4c2867ee
Sudhanshu <sudhanshu0pandey@gmail.com>
2025-01-03 07:43:01 +0530

    Convert into SPRING MVC project using xml + annotation both

---
+++ SpringWebXMLMaven/src/main/java/sud/learn/controller/HomeController.java
@@ -0,0 +1,16 @@
+package sud.learn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@ResponseBody
+	@GetMapping("/about")
+	public String showAboutPage() {
+		return "This is about page" ;
+	}
+
+}
